ee5cc7ed8cb615e74bad518457c71593a871c98c
Leonardo Gil Neves <leocomercial2016@gmail.com>
2022-10-23 22:02:48 -0300

    Estilização no pagination

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -10,7 +10,7 @@ import { map, tap } from 'rxjs/operators';
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1056'
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=600'
 
   constructor(
     private http: HttpClient
